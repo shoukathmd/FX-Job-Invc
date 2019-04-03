955df34f09b15ad85b0c8423fd5dd4fbf34d6211
sysadmin-imran <43744709+sysadmin-imran@users.noreply.github.com>
2019-04-03 10:15:25 +0530

    updated

--- fxp.ps1
+++ fxp.ps1
@@ -26,20 +26,20 @@ $headers = @{ Authorization = $basicAuthValue }
 ######################################################################################################################################
 
 #######################################################################################################################################
-#add-type @"
-#using System.Net;
-#using System.Security.Cryptography.X509Certificates;
-#public class TrustAllCertsPolicy : ICertificatePolicy {
-#    public bool CheckValidationResult(
-#        ServicePoint srvPoint, X509Certificate certificate,
-#        WebRequest request, int certificateProblem) {
-#        return true;
-#    }
-#}
-#"@
+add-type @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+    public bool CheckValidationResult(
+        ServicePoint srvPoint, X509Certificate certificate,
+        WebRequest request, int certificateProblem) {
+        return true;
+    }
+}
+"@
 $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
 [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
-#[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 
 
 
